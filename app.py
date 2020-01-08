01be51adc164fbaeb82e8d0f4c524d2e6cddab18
theoremoon <theoldmoon0602@gmail.com>
2020-01-08 09:55:48 +0900

    add face detection

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ if __name__ != '__main__':
 video = cv2.VideoCapture(0)
 video.set(cv2.CAP_PROP_FPS, 10)
 frame = None
+face_img = None
+cascade_path = "haarcascade_frontalface_alt.xml"
+cascade = cv2.CascadeClassifier(cascade_path)
 
 def get_frame():
     global frame
@@ -24,6 +27,18 @@ def get_frame():
         if not ok:
             return
         frame = jpg.tobytes()
+
+        threading.Thread(target=face_detect, args=[img]).start()
+
+def face_detect(img):
+    global face_img
+    grayscale_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    front_face_list = cascade.detectMultiScale(grayscale_img, minSize = (100, 100))
+
+    for (x,y,w,h) in front_face_list:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), thickness=10)
+    ok, jpg = cv2.imencode(".jpg", img)
+    face_img = jpg.tobytes()
 
 app = Flask(__name__)
 
@@ -38,9 +53,24 @@ def video_feed():
     return Response(generateFrames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+def generateFaceFrames():
+    while True:
+        time.sleep(0.5)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + face_img + b'\r\n\r\n')
+
+@app.route('/face.mjpeg')
+def face_feed():
+    return Response(generateFaceFrames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 @app.route('/')
 def index():
     return '<img src="/streaming.mjpeg" />'
+
+@app.route('/face')
+def face():
+    return '<img src="/face.mjpeg" />'
 
 
 threading.Thread(target=get_frame).start()
